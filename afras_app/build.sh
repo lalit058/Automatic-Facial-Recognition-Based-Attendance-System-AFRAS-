55f9a10073201145d9c331fe9ce71b8000bb0bab
lalit058 <lalitnegi2058@gmail.com>
2026-07-25 21:22:36 +0545

    Limit build parallel jobs to fix memory crash

--- afras_app/build.sh
+++ afras_app/build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
-# Install packages preferring wheels (prevents dlib compilation memory spike)
-pip install --no-cache-dir --prefer-binary -r requirements.txt
+# Limit C++ parallel compilation jobs to 1 to prevent RAM spikes (dlib / cmake)
+export CMAKE_BUILD_PARALLEL_LEVEL=1
+export MAX_JOBS=1
+
+# Install requirements without caching in RAM
+pip install --upgrade pip
+pip install --no-cache-dir -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
